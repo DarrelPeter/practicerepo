7577e443a3ed34128cf041fa64ebefab611a8829
DarrelPeter <darrelpeter7@gmail.com>
2024-06-27 11:49:31 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,4 +2,7 @@ public class  Test{
     public void display(){
         System.out.println("This is test class");
       }
+     public void display1(){
+         System.out.println("This method added after first commit");
+     }
 }
