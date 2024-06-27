aeed52c02e3fc8a025e5b6dfa63c454aef7ed54b
DarrelPeter <darrelpeter7@gmail.com>
2024-06-27 11:51:08 +0530

    deleted first method in Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,5 @@
 public class  Test{
-    public void display(){
-        System.out.println("This is test class");
-      }
+
      public void display1(){
          System.out.println("This method added after first commit");
      }
